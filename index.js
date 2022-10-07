56eaee4d02cb5dfef5a0788ae9d0a0ebbcfaab9c
junghwaK <jeonghwa054@gmail.com>
2022-10-07 17:46:02 +0900

    connect with mongodb

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 //main 
 
 //아래 둘다 가능하지만 import로 쓸것.
-import express from "express";
+import express from "express"
 // const express = require("express");
+import dotenv from "dotenv"
+import mongoose from "mongoose"
 
 const app = express()
+dotenv.config()
+
+const connect = async () => {
+    try{
+        await mongoose.connect(process.env.MONGO);
+        console.log("Connected to mongoDB")
+    } catch (error) {
+        throw error;
+    }
+};
+
+mongoose.connection.on("disconnected", () => {
+    console.log("mongoDB disconnected!")
+})
+
 
 app.listen(8000, ()=>{
+    connect()
     console.log("Connected to backend.")
 })
