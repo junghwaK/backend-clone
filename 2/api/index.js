ad68290bac22f0db7cebc8f68965135f7db9c962
junghwaK <jeonghwa054@gmail.com>
2022-10-13 18:28:22 +0900

    make register auth and save DB, crypt

--- 2/api/index.js
+++ 2/api/index.js
@@ -5,11 +5,32 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const helmet = require("helmet");
 const morgan = require("morgan");
+const userRoute = require("./routes/users.js");
+const authRoute = require("./routes/auth.js");
 
 dotenv.config();
+
 mongoose.connect(process.env.MONGO_URL, {useNewUrlParser : true}, ()=>{
     console.log("Connected to MongoDB")
 });
+
+//middleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
+
+//rest api만들거라서 아래는 필요가 없지
+
+// app.get("/", (req, res) => {
+//     res.send("Welcome to homepage")
+// })
+
+// app.get("/users", (req, res) => {
+//     res.send("Welcome to user page")
+// })
 
 
 app.listen(8080, () => {
