0f9698d9ee2d0ef641828dd05fb1239ba1fac584
junghwaK <jeonghwa054@gmail.com>
2022-10-13 14:15:56 +0900

    connect with mongoDB

--- 2/api/index.js
+++ 2/api/index.js
@@ -1,6 +1,16 @@
 const express = require("express");
-const { contentSecurityPolicy } = require("helmet");
+// const { contentSecurityPolicy } = require("helmet");
 const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
+
+dotenv.config();
+mongoose.connect(process.env.MONGO_URL, {useNewUrlParser : true}, ()=>{
+    console.log("Connected to MongoDB")
+});
+
 
 app.listen(8080, () => {
     console.log("Backend server is running!")
