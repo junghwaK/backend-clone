104d4cd7afc21fbae7368281f06cf6602cdb2118
junghwaK <jeonghwa054@gmail.com>
2022-10-17 00:35:29 +0900

    update the post

--- 2/api/routes/posts.js
+++ 2/api/routes/posts.js
@@ -1,5 +1,5 @@
 const router =  require("express").Router();
-const Post = require("../models /Post.js");
+const Post = require("../models/Post");
 
 //CREATE A POST
 router.post("/", async(req, res)=>{
@@ -10,8 +10,22 @@ router.post("/", async(req, res)=>{
     }catch(err){
         res.status(500).json(err)
     }
-})
+});
+
 //UPDATE A POST
+router.put("/:id", async (req, res) => {
+    try {
+      const post = await Post.findById(req.params.id);
+      if (post.userId === req.body.userId) {
+        await post.updateOne({ $set: req.body });
+        res.status(200).json("the post has been updated");
+      } else {
+        res.status(403).json("you can update only your post");
+      }
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
 //DELETE A POST
 //LIKE A POST
 //GET A POST
