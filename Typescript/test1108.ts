c94bbeda500ac6a7d42e4560a14ab22e320f299a
junghwaK <jeonghwa054@gmail.com>
2022-11-08 18:31:03 +0900

    Typescript basic : about type

---
+++ Typescript/test1108.ts
@@ -0,0 +1,27 @@
+let a = "hello"
+let b = false
+let c = [1, 2, 3]
+
+//명시적 표현을 하기도 하는데 최소한으로 하는것이 좋다.
+//그래야 우리가 키보드 치는 시간을 줄일 수 있다. 
+let d : number[] = [] 
+d.push(1)
+const player ={
+    name : "nico"
+}
+
+// syntax - do : and then type(name of a type)
+// you don't have to do like ' : number[] '
+// sometimes better do let typescript infer the type automatically.
+let e : number[] = [1, 2];
+
+
+//Optional Type
+const user : {
+    name:string,
+    age?:number
+} = {
+    name : "Jay"
+}
+
+
