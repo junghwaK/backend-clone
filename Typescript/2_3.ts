725aca92fc8370d95d0a0c8af6efee023a7e1c9d
junghwaK <jeonghwa054@gmail.com>
2022-11-10 10:51:20 +0900

    study: tuple

--- Typescript/2_3.ts
+++ Typescript/2_3.ts
@@ -15,3 +15,14 @@ nico.age = 12
 const numbers : readonly number[] = [1, 2, 3, 4]
 //'push' does not exist on read only. but if I remove the read only, it works
 numbers.push(1)
+
+//Tuple
+//It allow create Array. Tuple need to have minium length and also that should have certain specific types in certain specific position.
+const player1: [string, number, boolean] = ["Jay", 1, true]
+//When I go to he first index. And I try to change number, it won't be allow.
+//player1's first index should be always stting
+player1[0] = 1
+
+//can combine tuple and read only
+//const player1: readonly [string, number, boolean] = ["Jay", 1, true]
+
