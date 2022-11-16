0b43ef06375374ae22861f148753dc1c0abd39f3
junghwaK <jeonghwa054@gmail.com>
2022-11-16 16:15:19 +0900

    ts: call signature

---
+++ Typescript/3_1.ts
@@ -0,0 +1,7 @@
+//functions in Typescript
+//call signatures, polymorphism, overloading and generics
+
+//call signatures
+type Add = (a:number, b:number) => number;
+const add:Add = (a, b) => a+b
+
