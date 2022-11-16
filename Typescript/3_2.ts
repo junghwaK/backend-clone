34760fcd19f37756d82d1ddf4616b8152b5897d6
junghwaK <jeonghwa054@gmail.com>
2022-11-16 23:44:27 +0900

    ts: Polymorphism

--- Typescript/3_2.ts
+++ Typescript/3_2.ts
@@ -2,10 +2,15 @@
 // create a function, receive array and is going to print the items of array one by one(doesnt matter the type)
 type SuperPrint = {
     //concrete type : types we've seen before - number, boolean, string, void, unknown...
-    (arr: number[]):void
-    (arr: boolean[]):void
-    (arr: string[]):void
-    (arr: (number|boolean)[]):void
+    //<>in here, as you can see. this is the Generic. it could be the potato or whatever you want. 
+    //<> This called signature is going to receive Generic
+    <TypePlaceholder>(arr: TypePlaceholder[]):TypePlaceholder
+
+
+    // (arr: boolean[]):void
+    // (arr: string[]):void
+    // (arr: (number|boolean)[]):void
+
     //generic : it's like placeholder for the types.
     //We use generic when we are writing our call signatures, but we really dont know what type would be here.
 }
@@ -13,6 +18,7 @@ type SuperPrint = {
 const superPrint: SuperPrint = (arr) => {
     arr.forEach(i => console.log(i))
 }
+
 
 superPrint([1, 2,  3, 4])
 superPrint([true, false, true])
