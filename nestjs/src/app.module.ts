480037c7f80a545dcd41aa89f5b7a13cbed7a49f
junghwaK <jeonghwa054@gmail.com>
2022-11-22 19:30:05 +0900

    nestjs

---
+++ nestjs/src/app.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+
+
+//decorator (nest.js with decorator. can add functionality to a class)
+@Module({
+  imports: [],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
