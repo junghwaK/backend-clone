8f9bc1d2b7bc69df052d24a6ebbed65ce87f1d18
junghwaK <jeonghwa054@gmail.com>
2022-11-23 18:59:20 +0900

    nest

--- nestjs/src/app.module.ts
+++ nestjs/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { MoviesController } from './movies/movies.controller';
 
 
 //decorator (nest.js with decorator. can add functionality to a class)
 @Module({
   imports: [],
-  controllers: [],
+  controllers: [MoviesController],
   providers: [],
 })
 export class AppModule {}
