5ff5074ca958a972ad3bbabf1e5c21c674b5087f
junghwaK <jeonghwa054@gmail.com>
2022-11-30 16:06:21 +0900

    nest: install uuid and use unique id

--- nestjs-board/src/boards/boards.service.ts
+++ nestjs-board/src/boards/boards.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@nestjs/common';
-import { Board } from './board.model';
+import { Board, BoardStatus } from './board.model';
+//uuid는 버전이 많은데 그중에 우리는 v1을 쓴다.
+import { v1 as uuid } from 'uuid';
 
 @Injectable()
 export class BoardsService {
@@ -8,5 +10,18 @@ export class BoardsService {
 //여기의 Board는 return값이 어떤타입이 되는지
     getAllBoards(): Board[] {
         return this.boards;
+    }
+
+    createBoard(title: string, description: string){
+        const board: Board = {
+            //title과 description 처럼 똑같은거 두개써주면 그냥 하나로 써주면 된다. 
+            // title: title,
+            // description: description,
+            id: uuid(),
+            title,
+            description,
+            //model에 우리가 PUBLIC아님 PRIVATE 두개만 정의해줘서 이렇게 두개만 뜸
+            status: BoardStatus.PUBLIC
+        }
     }
 }
