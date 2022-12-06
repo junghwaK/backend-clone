ebe6c9763b24247cd0ad930d18e1a61f0ffdcda8
junghwaK <jeonghwa054@gmail.com>
2022-12-06 18:01:03 +0900

    js: Nested Loops

--- Javascript/193.js
+++ Javascript/193.js
@@ -8,4 +8,25 @@ for(let i = animals.length - 1; i >= 0; i--){
     console.log(animals[i])
 };
 
-//중첩루프(Nested Loops)
+//중첩루프(Nested Loops) : for loop안에 또다른 for loop존재
+for (let i = 1; i<= 10; i++){
+    console.log(`i is : ${i}`)
+    for(let j = 1; j<4; j++ ){
+        console.log(`       j is: ${j}`)
+    }
+}
+
+const seatingChart = [
+    ['Kristen', 'Erik', 'Namita'],
+    ['Geoffrey', 'Juanita', 'Antonio', 'Kevin'],
+    ['Yuma', 'Sakura', 'Jack', 'Erika']
+]
+
+for (let i = 0; i < seatingChart.length; i++){
+    // console.log(seatingChart[i]) -> 이건 그냥 한줄씩 출력 그렇다면 한줄 안에있는 한글자씩 빼려면?
+    const row = seatingChart[i];
+    console.log(`ROW #${i + 1}`);
+    for(let j = 0; j < row.length; j++){
+        console.log(row[j])
+    }
+}
